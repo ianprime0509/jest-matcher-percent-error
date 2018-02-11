ff96c08e58f5d010e8c5b83693b01631d2cb20e1
Ian Johnson <ianprime0509@gmail.com>
2018-02-11 13:50:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 Ian Johnson
+ *
+ * This is free software, distributed under the MIT license.  A copy of the
+ * license can be found in the LICENSE file in the project root, or at
+ * https://opensource.org/licenses/MIT.
+ */
+/**
+ * Check whether the actual value is within the given percent error of the
+ * expected value.
+ *
+ * The percent error check is inclusive.
+ *
+ * @param {number} actual
+ * @param {number} expected
+ * @param {number} percent
+ * @return {{message: (function(): string), pass: boolean}}
+ */
+export default function toBeWithinPercent(actual, expected, percent) {
+  const secondArgument = 'percent';
+  this.utils.ensureNumbers(actual, expected, '.toBeWithinPercent');
+
+  // If the expected value is zero, we'll be dividing by zero in the
+  // percentError function.
+  if (expected === 0) {
+    throw new Error(
+      this.utils.matcherHint('[.not].toBeWithinPercent', undefined, undefined, {
+        secondArgument
+      }) +
+      '\n\n' +
+      'Expected value cannot be zero.'
+    );
+  }
+
+  const error = percentError(actual, expected);
+  const pass = error <= percent;
+
+  const message = pass
+    ? () =>
+      this.utils.matcherHint('.not.toBeWithinPercent', undefined, undefined, {
+        secondArgument
+      }) +
+      '\n\n' +
+      `Expected value not to be within ${this.utils.printExpected(
+        percent,
+      )}% of:\n` +
+      `  ${this.utils.printExpected(expected)}\n` +
+      'Received:\n' +
+      `  ${this.utils.printReceived(actual)} (${this.utils.printReceived(
+        error,
+      )}% error)`
+    : () =>
+      this.utils.matcherHint('.toBeWithinPercent', undefined, undefined, {
+        secondArgument
+      }) +
+      '\n\n' +
+      `Expected value to be within ${this.utils.printExpected(
+        percent,
+      )}% of:\n` +
+      `  ${this.utils.printExpected(expected)}\n` +
+      'Received:\n' +
+      `  ${this.utils.printReceived(actual)} (${this.utils.printReceived(
+        error,
+      )}% error)`;
+
+  return { message, pass };
+}
+
+/**
+ * Return the percent error of the given actual value, relative to the given
+ * expected value.
+ */
+function percentError(actual, expected) {
+  return 100 * Math.abs(actual - expected) / expected;
+}
---
+++ src/index.test.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Ian Johnson
+ *
+ * This is free software, distributed under the MIT license.  A copy of the
+ * license can be found in the LICENSE file in the project root, or at
+ * https://opensource.org/licenses/MIT.
+ */
+import toBeWithinPercent from '.';
+
+expect.extend({ toBeWithinPercent });
+
+describe('.toBeWithinPercent()', () => {
+  test('correctly identifies passing cases', () => {
+    expect(5).toBeWithinPercent(5, 1);
+    expect(110).toBeWithinPercent(100, 10);
+    expect(105).toBeWithinPercent(101, 5);
+    expect(-10).toBeWithinPercent(-15, 60);
+    expect(1).toBeWithinPercent(1, 0);
+    expect(0).toBeWithinPercent(5, 150);
+  });
+
+  test('correctly identifies failing cases', () => {
+    expect(5).not.toBeWithinPercent(1, 100);
+    expect(-1).not.toBeWithinPercent(2, 50);
+  });
+
+  test('fails when the expected value is zero', () => {
+    expect(() => expect(5).toBeWithinPercent(0, 100)).toThrow();
+  });
+
+  test('fails when the actual or expected value is not a number', () => {
+    expect(() => expect('hi').toBeWithinPercent(5, 50)).toThrow();
+    expect(() => expect(6).toBeWithinPercent([6], 90)).toThrow();
+    expect(() => expect({}).toBeWithinPercent([], 1)).toThrow();
+  });
+});
